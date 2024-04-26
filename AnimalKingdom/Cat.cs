e135fece66d69eaff8252a66086d86a927291efc
duducg <edu.sang.rocha@gmail.com>
2024-04-26 16:30:10 +0100

    Added cat nipples

--- AnimalKingdom/Cat.cs
+++ AnimalKingdom/Cat.cs
@@ -1,10 +1,16 @@
 ﻿namespace AnimalKingdom
 {
-    public class Cat : Animal
+    public class Cat : Animal, IMammal
     {
+        public int NumberOfNipples 
+        {
+            get { return 6;}
+        }
+
         public override string Sound()
         {
             return base.Sound() + "Miau";
         }
+        
     }
 }
