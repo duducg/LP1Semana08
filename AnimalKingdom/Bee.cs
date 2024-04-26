c9ff33d5f6457e457da33e0c3ee7eea014e23e3f
duducg <edu.sang.rocha@gmail.com>
2024-04-26 15:14:49 +0100

    Added Bat and Bee

---
+++ AnimalKingdom/Bee.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimalKingdom
+{
+    public class Bee :Animal
+    {
+        public override string Sound()
+        {
+            return base.Sound() + "Bzzz";
+        } 
+        
+    }
+}
