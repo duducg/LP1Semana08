ade50c40c32ff886f380b9c0922bd20d16b6f6d8
duducg <edu.sang.rocha@gmail.com>
2024-04-26 16:29:36 +0100

    Implemented IMammal and initialized nipples

--- AnimalKingdom/Bat.cs
+++ AnimalKingdom/Bat.cs
@@ -5,8 +5,12 @@ using System.Threading.Tasks;
 
 namespace AnimalKingdom
 {
-    public class Bat : Animal
+    public class Bat : Animal, IMammal
     {
+        public int NumberOfNipples 
+        {
+            get { return 4;}
+        }
         public override string Sound()
         {
             return base.Sound() + "Click";
--- AnimalKingdom/Dog.cs
+++ AnimalKingdom/Dog.cs
@@ -1,7 +1,11 @@
 ﻿namespace AnimalKingdom
 {
-    public class Dog : Animal
+    public class Dog : Animal, IMammal
     {
+        public int NumberOfNipples 
+        {
+            get { return 8 ;}
+        }
         public override string Sound()
         {
             return base.Sound() + "Woof!";
